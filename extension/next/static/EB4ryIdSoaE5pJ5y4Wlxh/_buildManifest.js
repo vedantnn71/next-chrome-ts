102d81bbdc88598ab46ebb6c4984faadd76c5b43
Vedant Nandwana <vedantnn7.git@gmail.com>
2022-08-04 09:29:05 +0530

    Ignore files produced during build

--- extension/next/static/EB4ryIdSoaE5pJ5y4Wlxh/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/149b18973e5508c7.css","static/chunks/pages/index-25c4be120fefef40.js"],"/_error":["static/chunks/pages/_error-a03fb240090ad5de.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
